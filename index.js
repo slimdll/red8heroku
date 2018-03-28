4914f355b1ea384d4966c3d72c952eaa389500af
slimdll <37386769+slimdll@users.noreply.github.com>
2018-03-28 14:05:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require ('node-telegram-bot-api')
+
+const TOKEN = '593469041:AAECycHv1PKrHOPZ9l-XMrtDc44Q7vFyiLM'
+ 
+const bot = new TelegramBot (TOKEN, {polling: true})
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'Hello Red8, bot says: "Hi, $(message.from.first_name}"')
+
+})
