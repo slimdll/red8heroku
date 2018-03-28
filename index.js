b10447ecfc3bb218bbf788817a4a01472529bb69
slimdll <37386769+slimdll@users.noreply.github.com>
2018-03-28 13:49:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const TOKEN = '593469041:AAFuTMg63AR9I3TJJYQ_dMydmLC8QbPrzDg'
 const bot = new TelegramBot (TOKEN, {polling: true})
 
 bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'Hello Red8, bot says: "Hi, $(message.from.first_name)"'
+  bot.sendMessage(msg.chat.id, 'Hello Red8, bot says: "Hi, $(message.from.first_name)"'}
 
 })
