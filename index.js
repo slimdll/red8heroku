576ca3088a67f30e67520377909cc79ac1432a9e
slimdll <37386769+slimdll@users.noreply.github.com>
2018-04-06 04:50:14 +0300

    Update and rename _index.js to index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const  TelegramBot  =  require ( 'node-telegram-bot-api' )
+
+const  TOKEN  =  '529348970: AAFe1xIkfLObyiimeLAqugep90eX3kii66U
+const   bot   =   new   TelegramBot ( TOKEN , { polling : true })
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'Hi, RED8/Welcome')
+
+})
